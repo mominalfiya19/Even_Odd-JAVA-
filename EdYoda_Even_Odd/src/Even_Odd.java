05e44c83c047010dea067f205e83d21554d97647
mominalfiya19 <91407480+mominalfiya19@users.noreply.github.com>
2021-10-18 20:06:47 +0530

    Add files via upload

---
+++ EdYoda_Even_Odd/src/Even_Odd.java
@@ -0,0 +1,17 @@
+
+public class Even_Odd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+   int a=15;
+   if(a%2==0)
+   		{
+	   System.out.println("Given number is Even");
+   		}
+   else
+   		{
+	   System.out.println("Given number is Odd");
+   		}
+	}
+
+}
